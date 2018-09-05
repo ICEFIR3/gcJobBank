2577815a46d73bd5a2eb988f6cea027dcf9c1bb1
Ken Morency <ken.morency@servicecanada.gc.ca>
2018-09-05 13:19:18 -0400

    WIPE

--- README.md
+++
@@ -1,2 +0,0 @@
-# gcJobBank
-Google action for Job Bank
--- functions/index.js
+++
@@ -1,8 +0,0 @@
- 
-'use strict';
-
-const functions = require('firebase-functions');
-const app = require('./gcJobBankApp');
-let items = [];
-module.exports.gcJobBankAction = functions.https.onRequest(app);
- 
